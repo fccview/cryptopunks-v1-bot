e54d85d7a16671c38129e7b9d424d7e1080faa1e
Admin <admin@Admins-MacBook-Pro.local>
2023-02-06 14:10:49 +0000

    ai character length limitation fix

--- commands/ai.js
+++ commands/ai.js
@@ -1,10 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const Discord = require("discord.js");
-const { gptKey } = require('../config.json');
+const { openaiKey } = require('../config.json');
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: gptKey,
+    apiKey: openaiKey,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -12,8 +11,8 @@ const ask = async (prompt) => {
   const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      temperature: 1,
-      max_tokens: 700,
+      temperature: 0.7,
+      max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -22,49 +21,39 @@ const ask = async (prompt) => {
   return answer;
 }
 
-const chunkString = (str, length) => {
-    return str.match(new RegExp('.{1,' + length + '}', 'g'));
-}
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ai')
-        .setDescription(`Let the CryptoPunks V1 Bot AI answer your questions. Powered by ChatGPT`)
+        .setDescription(`Let the V1 Punks Bot AI answer your questions. Powered by openai`)
         .addStringOption(option =>
             option.setName('input')
                 .setDescription('Ask something')
                 .setRequired(true)
         ),
 
-    chunkString(str, length) {
-        return str.match(new RegExp('.{1,' + length + '}', 'g'));
-    },
-
     async execute(interaction) {
-        await interaction.deferReply({ephemeral: false});
-        const prompt = interaction.options.getString('input');
-        const answer = await ask(prompt);
+        if (openaiKey) {
+            if (answer.length > 1999) {
+                if (answer.length > 4095) {
+                    const chunks = chunkString(answer, 4095);
+                    const embeds = chunks.map((str) => {
+                        return new Discord.MessageEmbed()
+                            .setColor("RANDOM")
+                            .setDescription(str)
+                        });
 
-        console.log(answer.length);
-
-        if (answer.length > 1999) {
-            if (answer.length > 4095) {
-                const chunks = chunkString(answer, 4095);
-                const embeds = chunks.map((str) => {
-                    return new Discord.MessageEmbed()
+                    await interaction.editReply({embeds: embeds});         
+                } else {
+                    const embed = new Discord.MessageEmbed()
                         .setColor("RANDOM")
-                        .setDescription(str)
-                    });
-
-                await interaction.editReply({embeds: embeds});         
+                        .setDescription(answer);
+                    await interaction.editReply({embeds: [embed]})
+                }
             } else {
-                const embed = new Discord.MessageEmbed()
-                    .setColor("RANDOM")
-                    .setDescription(answer);
-                await interaction.editReply({embeds: [embed]})
-            }
+                await interaction.editReply({content: answer});
+            }       
         } else {
-            await interaction.editReply({content: answer});
-        }        
+            interaction.reply({content: 'The author has not set an openai api key.', ephemeral: true});
+        }
     },
 };
